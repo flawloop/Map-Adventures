89510a7f7e36d849cc4488fa3516991ad414c891
flawloop <khusravkhamidov@gmail.com>
2022-09-05 19:42:40 +0600

    Initial Commit

---
+++ MapAdventures/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MapAdventures
+//
+//  Created by yaaburnee on 05/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MapAdventures/MapAdventuresApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAdventuresApp.swift
+//  MapAdventures
+//
+//  Created by yaaburnee on 05/09/22.
+//
+
+import SwiftUI
+
+@main
+struct MapAdventuresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
